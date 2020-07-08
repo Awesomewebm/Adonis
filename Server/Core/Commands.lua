efe9508fb27eabe5a0cbf8a8e6495bd5f194d13c
Awesomewebm <olucasps@gmail.com>
2020-07-08 15:23:51 -0400

    Add :unteam
    This is an issue I have seen before, and I have before where you cannot take a player off of a team after putting them on a team

--- Server/Core/Commands.lua
+++ Server/Core/Commands.lua
@@ -6261,6 +6261,24 @@ return function()
 			end
 		};
 		
+		Unteam = {
+			Prefix = server.Settings.Prefix;
+			Commands = {"unteam"};
+			Args = {"player"};
+			Description = "Sets the target player(s) to neutral";
+			Hidden = false; 
+			Fun = false;
+			AdminLevel = "Moderators";
+			Function = function(plr,args)
+				for _,player in ipairs(server.Functions.GetPlayers(plr, args[1])) do
+					
+					player.Neutral = true
+					player.Team = nil
+					player.TeamColor = BrickColor.new(194) -- Neutral Team
+				end
+			end
+			};
+		
 		SetFOV = {
 			Prefix = Settings.Prefix;
 			Commands = {"fov";"fieldofview";"setfov"};
